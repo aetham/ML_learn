18ec0447fc0474ae376f76ebc2afb1b0f2f0cabc
aetham <henri206@hotmail.com>
2021-04-30 12:23:04 +0300

    v02

---
+++ fourth.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+train_data = pd.read_csv("Titan data/train.csv")
+print(train_data.head())
+
+test_data = pd.read_csv("Titan data/test.csv")
+print(test_data.head())
+
+women = train_data.loc[train_data.Sex == 'female']["Survived"]
+rate_women = sum(women)/len(women)
+print("% of women who survived:", rate_women)
+
+men = train_data.loc[train_data.Sex == 'male']["Survived"]
+rate_men = sum(men)/len(men)
+print("% of women who survived:", rate_men)
+
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('my_submission.csv', index=False)
+print("Your submission was successfully saved!")
